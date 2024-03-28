43c640f56e03e8504db56a1285cc86692a13b7b2
Sasanka Roy <sasankaroy033@gmail.com>
2024-03-28 00:54:30 -0700

    small change

--- src/component/contact/ContactUs.jsx
+++ src/component/contact/ContactUs.jsx
@@ -68,64 +68,128 @@ export const ContactUs = () => {
 
     // Up text with animation...
 
-        // for the big "U" ...
-            animate(
-            "#gridTile_44",
-            { backgroundColor: firstBgColors },
-            { duration: 0.8, times: timings }
-            );
+    // for the big "U" ...
+    animate(
+      "#gridTile_43",
+      { backgroundColor: 'red' },
+      { duration: 0.8, times: timings }
+    );
 
-            animate(
-            "#gridTile_64",
-            { backgroundColor: firstBgColors },
-            { duration: 0.8, times: timings }
-            );
+    animate(
+      "#gridTile_63",
+      { backgroundColor: 'red' },
+      { duration: 0.8, times: timings }
+    );
 
-            animate(
-            "#gridTile_84",
-            { backgroundColor: firstBgColors },
-            { duration: 0.8, times: timings }
-            );
+    animate(
+      "#gridTile_83",
+      { backgroundColor: 'red' },
+      { duration: 0.8, times: timings }
+    );
 
-            animate(
-            "#gridTile_104",
-            { backgroundColor: firstBgColors },
-            { duration: 0.8, times: timings }
-            );
+    animate(
+      "#gridTile_103",
+      { backgroundColor: 'red' },
+      { duration: 0.8, times: timings }
+    );
+    animate(
+      "#gridTile_123",
+      { backgroundColor: 'red' },
+      { duration: 0.8, times: timings }
+    );
 
-            animate(
-            "#gridTile_105",
-            { backgroundColor: firstBgColors },
-            { duration: 0.8, times: timings }
-            );
+    animate(
+        "#gridTile_124",
+        { backgroundColor: 'red' },
+        { duration: 0.8, times: timings }
+      );
 
-            animate(
-            "#gridTile_106",
-            { backgroundColor: firstBgColors },
-            { duration: 0.8, times: timings }
-            );
-            
-            animate(
-            "#gridTile_86",
-            { backgroundColor: firstBgColors },
-            { duration: 0.8, times: timings }
-            );
+    animate(
+      "#gridTile_125",
+      { backgroundColor: 'red' },
+      { duration: 0.8, times: timings }
+    );
 
-            animate(
-            "#gridTile_66",
-            { backgroundColor: firstBgColors },
-            { duration: 0.8, times: timings }
-            );
-            
-            animate(
-            "#gridTile_46",
-            { backgroundColor: firstBgColors },
-            { duration: 0.8, times: timings }
-            );
-        // for the big "P"....
-            
-            // animate()    
-            
+    animate(
+      "#gridTile_106",
+      { backgroundColor: 'red' },
+      { duration: 0.8, times: timings }
+    );
+    animate(
+      "#gridTile_126",
+      { backgroundColor: 'red' },
+      { duration: 0.8, times: timings }
+    );
+
+    animate(
+      "#gridTile_86",
+      { backgroundColor: 'red' },
+      { duration: 0.8, times: timings }
+    );
+
+    animate(
+      "#gridTile_66",
+      { backgroundColor: 'red' },
+      { duration: 0.8, times: timings }
+    );
+
+    animate(
+      "#gridTile_46",
+      { backgroundColor: 'red' },
+      { duration: 0.8, times: timings }
+    );
+    // for the big "P"....
+
+    animate(
+      "#gridTile_48",
+      { backgroundColor: firstBgColors },
+      { duration: 0.8, times: timings }
+    );
+    animate(
+      "#gridTile_68",
+      { backgroundColor: firstBgColors },
+      { duration: 0.8, times: timings }
+    );
+    animate(
+      "#gridTile_88",
+      { backgroundColor: firstBgColors },
+      { duration: 0.8, times: timings }
+    );
+    animate(
+      "#gridTile_108",
+      { backgroundColor: firstBgColors },
+      { duration: 0.8, times: timings }
+    );
+    animate(
+      "#gridTile_128",
+      { backgroundColor: firstBgColors },
+      { duration: 0.8, times: timings }
+    );
+    animate(
+      "#gridTile_49",
+      { backgroundColor: firstBgColors },
+      { duration: 0.8, times: timings }
+    );
+    animate(
+      "#gridTile_50",
+      { backgroundColor: firstBgColors },
+      { duration: 0.8, times: timings }
+    );
+    animate(
+      "#gridTile_70",
+      { backgroundColor: firstBgColors },
+      { duration: 0.8, times: timings }
+    );
+    animate(
+      "#gridTile_90",
+      { backgroundColor: firstBgColors },
+      { duration: 0.8, times: timings }
+    );
+    animate(
+      "#gridTile_89",
+      { backgroundColor:firstBgColors},
+      { duration: 0.8, times: timings }
+    );
   };
 
   useEffect(() => {
